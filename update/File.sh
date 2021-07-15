47f300910374d072295054caba4b9deaf27241a9
lesta-1 <82885098+lesta-1@users.noreply.github.com>
2021-07-15 20:47:16 +0700

    Create File.sh

---
+++ update/File.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+IP=$(wget -qO- icanhazip.com);
+domain=$(cat /var/lib/premium-script/ipvps.conf)
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+hari="0"
+Pass=1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+clear
+useradd -e
